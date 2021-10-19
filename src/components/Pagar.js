caddf28ce6d800f5a823250863e96ee67109cf32
edwardpi12 <edward.pizzurro@equilibrio.agency>
2021-10-18 22:19:56 -0400

    Subir Facturas a Firebase y traerlas para mostrarlas en la página

--- src/components/Pagar.js
+++ src/components/Pagar.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { db } from '../firebase';
 
 // Importación del react-router-dom
 import {
@@ -8,12 +9,24 @@ import {
 
 function Pagar() {
 
-    // const [monto , setMonto] = useState(null)
+    const initializeStateValues = {
+        descripcion: "",
+        monto: ""
+    };
 
-    // function guardarMonto(event) {
-    //     setMonto(event.target.value)
-    //     console.log(monto)
-    // }
+    const [values, setValues] = useState(initializeStateValues);
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setValues({...values, [name]: value})
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await db.collection('facturas').doc().set(values);
+        console.log('new factura');
+        setValues({...initializeStateValues});
+    };
 
     return (
         <div className="container d-flex justify-content-center h-100 align-items-center">
@@ -23,17 +36,17 @@ function Pagar() {
                     <h4 className="card-title ">Ventana de Pago</h4>
                     <br/>
                     <h6 className="card-title ">Descripción</h6>
-                    <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder="Ejemplo: 5 materias, mes de octubre"></input> 
+                    <input value={values.descripcion} name="descripcion" onChange={handleInputChange} type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder="Ejemplo: 5 materias, mes de octubre"></input> 
                     <br/>
                     <h6 className="card-title ">Monto</h6>
                     <div className="input-group mb-3">
                         <span className="input-group-text">$</span>
-                        <input type="text" class="form-control" placeholder="Ejemplo: 375"></input>
+                        <input value={values.monto} name="monto" onChange={handleInputChange} type="text" class="form-control" placeholder="Ejemplo: 375"></input>
                         <span className="input-group-text">USDT</span>
                     </div>
                     <p className="text-white">Para proceder por favor haz click en Pagar</p>
                     <Router>
-                        <Link to="/CoinPayments" className="btn btn-outline-secondary rounded-0" target="_blank">
+                        <Link onClick={handleSubmit} className="btn btn-outline-secondary rounded-0" target="_blank">
                             Pagar
                         </Link>
                     </Router>
