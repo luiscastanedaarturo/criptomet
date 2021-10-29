eec8ddd3d12c573c05031077504eebf31e0bbba7
Luis <luis.castaneda.arturo@gmail.com>
2021-10-28 22:14:13 -0400

    Bien

--- src/components/Factura.js
+++ src/components/Factura.js
@@ -2,14 +2,30 @@ import React from 'react'
 
 import "./Guias.css"
 
-function Factura({title, text}) {
+import articulo1 from "../assets/Banner Facturas.jpg"
+
+function Factura({title, text, fecha, nombre, cedula}) {
     return (
         <div className="container d-flex justify-content-center h-100 align-items-center">
           <div className="card mb-3 card text-left bg-dark animate__animated animate__fadeInUp">
+          <img src={articulo1} className="img-fluid" alt="..."/>
             <div className="card-body text-light">
-              <h5 className="card-title">{title}</h5>
-              <p className="card-text">{text}</p>
-              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                  
+              <div className="row">
+                <h4 className="card-title col"><small className="text-muted">Estudiante: {nombre}</small></h4>
+                <h4 className="card-title col"><small className="text-muted">Cédula: {cedula}</small></h4>
+              </div>
+
+              <div className="row">
+                  <h5 className="card-text col">Monto: {title} USDT</h5>
+                  <p className="card-text col">Descripción: {text}</p>
+              </div>
+
+              <div className="row">
+                <h6 className="card-text col"><small className="text-muted">Fecha: {fecha}</small></h6>
+              </div>
+
+              
             </div>
           </div>
         </div>
